7cac6ba40116f7683a22441b58c19a1c84a03cb2
lancestrom <dikakewren@gmail.com>
2025-10-09 11:53:56 +0700

    Initial commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Linktree Saya</title>
+	<link rel="stylesheet" href="<?= base_url() ?>assets/styles.css">
+</head>
+
+<body>
+	<div class="container">
+		<header>
+			<div class="profile">
+				<img src="https://bkk.smkth-jakbar.com/assets/img/bkk_terbaru.png" alt="Foto Profil" class="profile-img">
+				<h2>BKK</h2>
+				<h2>SMK TUNAS HARAPAN JAKARTA BARAT</h2>
+			</div>
+		</header>
+
+
+		<div class="links">
+
+
+
+			<a href="https://drive.google.com/file/d/1Gg-cNdtvZMLTwuMrPd_rWV2gszhCWfZj/view?usp=sharing" target="_blank" class="link-item">PROPOSAL JOB FAIR & EDU FAIR - PERUSAHAAN</a>
+			<a href="https://drive.google.com/file/d/1sk3IZkMEdr6aXs_u7GiHCEVyepLsTxvP/view?usp=sharing" target="_blank" class="link-item">PROPOSAL JOB FAIR & EDU FAIR - UNIVERSITAS</a>
+			<a href="https://drive.google.com/file/d/1XpQW0mBGE2PwOYTT7YI97GtP3CB8Rf6X/view?usp=sharing" target="_blank"
+				class="link-item">FORMULIR KESEDIAAN - PERUSAHAAN</a>
+			<a href="https://drive.google.com/file/d/120kBhkNxP5x7xkonEwcq5IuCEFbJANf5/view?usp=drive_link" target="_blank"
+				class="link-item">FORMULIR KESEDIAAN - UNIVERSITAS</a>
+			<a href="https://www.instagram.com/smkth.jakbar/?hl=id" target="_blank" class="link-item">INSTAGRAM</a>
+			<a href="https://www.tiktok.com/@bkk.smk.tunas.har" target="_blank" class="link-item">TIKTOK</a>
+		</div>
+
+		<footer>
+			<p>&copy; BKK SMK TUNAS HARAPAN</p>
+		</footer>
+	</div>
+
+	<!-- <script src="script.js"></script> -->
+</body>
+
+</html>
